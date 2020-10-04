8aaf34d7af1fc8a149a7b7d350adf0af9c1bd2e5
gadgetusaf <chris.burton@chrisgburton.com>
2020-10-04 13:02:34 -0400

    Update VPN-Down-email.ps1

--- Scripts/VPN-Down-email.ps1
+++ Scripts/VPN-Down-email.ps1
@@ -18,52 +18,29 @@ $subject = "Host Unreachable Alarm Scipt Startting"
 $body = "Starting Script"
 $Credential = Import-CliXml -Path ("$credpath")
 Send-EmailUpdate -credential $Credential -Server smtp.gmail.com -port 587 -from $from -to $to -subject $subject -body "$body"
-      
-
-
 # Start of monitoring loop
 Do {
-
     # Starts Test-NetConnection, looking for failed pings
-
     Do {
-
         $PingComputer1 = (Test-NetConnection $IPAddress).PingSucceeded
         $PingComputer2 = (Test-NetConnection $IPAddress).PingSucceeded
-
-        $PingComputer1
-        $PingComputer2
-
         Sleep 10
     }
-
     # When ping fails, it will send a notification email.
     Until (($PingComputer1 -eq $false) -and ($PingComputer2 -eq $false))
-
     # Send email notification that connection has been lost
        $Subject = "Host Unreachable ALERT"
        $Body = "Host is unreachable Failed to reach $IPAddress"
        Send-EmailUpdate -credential $Credential -Server smtp.gmail.com -port 587 -from $from -to $to -subject $subject -body "$body"
-
-
     Do {
         $PingComputer1 = (Test-NetConnection $IPAddress).PingSucceeded
         $PingComputer2 = (Test-NetConnection $IPAddress).PingSucceeded
-
-        $PingComputer1
-        $PingComputer2
-
         Sleep 10
     }
-
     # When ping is successful, it will send a notification email.
     Until (($PingComputer1 -eq $true) -and ($PingComputer2 -eq $true))
-
     # Send email notification that connection is back online
        $Subject = "Connection RESTORED"
        $Body = "restored"
        Send-EmailUpdate -credential $Credential -Server smtp.gmail.com -port 587 -from $from -to $to -subject $subject -body "$body"
-
-}
-
-Until (1 -eq 0) # end of monitoring loop, which never terminates.
+} Until (1 -eq 0) # end of monitoring loop, which never terminates.
